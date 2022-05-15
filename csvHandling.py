26735be072fa5e6e2ae1c9d115fe01bfae39ccee
Dylan <Dylan@DESKTOP-A456SH9>
2022-05-15 15:17:48 -0400

    1517 @ May 15 2022

--- csvHandling.py
+++ csvHandling.py
@@ -1,24 +1,54 @@
 import csv
 import subprocess
-import os
+#import os --will be implemented at a later time
 
-fileIndex = []
+fileIndex: list = []
+headings: list = ["FILE_NAME", "FILE_PATH"]
 
-def addFile():
-    file_name = input("File name: ")
+def addFile() -> None:
+    '''
+        Add a file to a CSV document. Allows you to name as well.
+    '''
+
+    file_name: str = input("File name: ")
     print("NOTE: YOUR FILE PATH MUST HAVE \\\\ IN PLACE OF / OR \\")
-    file_path = input("File path: ")
-
-    headings = ["FILE_NAME", "FILE_PATH"]
+    file_path: str = input("File path: ")
     
     with open("file_paths.csv", 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=headings)
         writer.writerow({"FILE_NAME": file_name, "FILE_PATH": file_path})
 
-def removeFile():
-    pass
+def removeFile() -> None:
+    '''
+        There is apparently no way to explicity remove a certain part of the CSV document without having to rewrite the whole file
+        We will do this as such:
+            1. Copy the file into a list format
+            2. Query the file name
+            3. Delete the list element
+            4. Rewrite to the CSV file
 
-def showCSVContents():
+        Output of DictReader as implied by logic:
+            - Outputted as a list
+            - Rows are formatted as KEY, VALUE pairs (DICT datatype)
+    '''
+
+    showCSVContents()
+    print("Which data do you wish to remove (ex: 0): ")
+
+    selector:int = int(input())
+    fileIndex.pop(selector)
+
+    with open("file_paths.csv", 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headings)
+
+        writer.writeheader()
+
+        for data in fileIndex:
+            print(data)
+            writer.writerow({"FILE_NAME": data[0], "FILE_PATH": data[1]})
+
+
+def showCSVContents() -> None:
     global fileIndex
     fileIndex = []
 
@@ -31,7 +61,9 @@ def showCSVContents():
             fileIndex.append([row["FILE_NAME"], row["FILE_PATH"]])
             iterator += 1
 
-def startFile():
+    print(fileIndex)
+
+def startFile() -> None:
 
     showCSVContents()
     
@@ -46,13 +78,13 @@ def startFile():
     '''
     
     print("\n Which file do you want to access? [example: 0]\n")
-    print("File access code: ")
+    print("File access code (any integer greater than or equal to zero): ")
 
     prompt_input: int = int(input()) # Will add syntax-checking for this later
-    
+
     print("Starting: " + fileIndex[prompt_input][0] + " from " + fileIndex[prompt_input][1])
 
-    process = None
+    process: subprocess.Popen = None
 
     try:
         process = subprocess.Popen(['start', fileIndex[prompt_input][1]], shell=True)
